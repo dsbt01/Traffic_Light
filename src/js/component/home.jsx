94de05f2741b0c143373f249b02edcbf3325c5cd
David Suarez <dsuarez001@att.net>
2022-10-24 20:50:27 -0400

    done

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,18 +1,32 @@
-import React, { useState, useEffect }  from "react";
-
+import React, { useState, useEffect } from "react";
 
 //create your first component
 const Home = () => {
-	const [selectedColor, setSelectedColor] = useState("red");
+  const [selectedColor, setSelectedColor] = useState("red");
 
-	return (
-		<div className="main-traffic-light">
-			<div className={"light redLight" + ((selectedColor === "red") ? " glowEffect" : "")}></div>
-			<div className={"light yellowLight"  + ((selectedColor === "yellow") ? " glowEffect" : "")} ></div>
-			<div className={"light greenLight"  + ((selectedColor === "green") ? " glowEffect" : "")} ></div>			
-		</div>
-	);
+  return (
+    <div className="main-traffic-light">
+      <div
+        onClick={() => setSelectedColor("red")}
+        className={
+          "light redLight" + (selectedColor === "red" ? " glowEffect" : "")
+        }
+      ></div>
+      <div
+        onClick={() => setSelectedColor("yellow")}
+        className={
+          "light yellowLight" +
+          (selectedColor === "yellow" ? " glowEffect" : "")
+        }
+      ></div>
+      <div
+        onClick={() => setSelectedColor("green")}
+        className={
+          "light greenLight" + (selectedColor === "green" ? " glowEffect" : "")
+        }
+      ></div>
+    </div>
+  );
 };
 
 export default Home;
- 
